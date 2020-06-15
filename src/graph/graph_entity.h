38ac1b9059f74180ac79b2d2d0426eb9c2e5e6b8
Nathan Scott <nathans@redhat.com>
2020-06-15 16:55:06 +1000

    Update graph_entity.h header with gcc v10 'extern' fixes
    Signed-off-by: Nathan Scott <nathans@redhat.com>

--- src/graph/graph_entity.h
+++ src/graph/graph_entity.h
@@ -5,7 +5,7 @@
 #define INVALID_ENTITY_ID -1l
 
 
-SIValue *PROPERTY_NOTFOUND;
+extern SIValue *PROPERTY_NOTFOUND;
 
 typedef struct {
     char *name;
@@ -22,14 +22,14 @@ typedef struct {
  * prop_count - number of new properties to add 
  * keys - array of properties keys 
  * values - array of properties values */
-void GraphEntity_Add_Properties(GraphEntity *e, int prop_count, char **keys, SIValue *values);
+extern void GraphEntity_Add_Properties(GraphEntity *e, int prop_count, char **keys, SIValue *values);
 
 /* Retrieves entity's property
  * NOTE: If the key does not exist, we return the special
  * constant value PROPERTY_NOTFOUND. */
-SIValue* GraphEntity_Get_Property(const GraphEntity *e, const char* key);
+extern SIValue* GraphEntity_Get_Property(const GraphEntity *e, const char* key);
 
 /* Release all memory allocated by entity */
-void FreeGraphEntity(GraphEntity *e);
+extern void FreeGraphEntity(GraphEntity *e);
 
 #endif
